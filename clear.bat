9bf16a735638a32ba3687293fff5fb7ffa8f27fc
shiningrise <shiningrise@gmail.com>
2013-03-10 19:15:56 +0800

    完善自动构建

---
+++ clear.bat
@@ -0,0 +1,7 @@
+::echo off
+::del /q/a/f/s Release
+rd /s/q Release
+::del /q/a/f/s Setup
+rd /s/q Setup
+del /q/a/f/s *.zip
+pause
--- tools/build.bat
+++ tools/build.bat
@@ -3,6 +3,8 @@ cd ..\
 mkdir Release
 cd tools
 path %SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319\
- msbuild.exe ..\src\MidExam.sln /t:Rebuild /p:Configuration=Release /l:FileLogger,Microsoft.Build.Engine;logfile=Build1.log
+msbuild.exe ..\src\MidExam.sln /t:Rebuild /p:Configuration=Release /l:FileLogger,Microsoft.Build.Engine;logfile=Build1.log
+msbuild.exe ..\src\WebSetup1\WebSetup1.vdproj /t:Rebuild /p:Configuration=Release /l:FileLogger,Microsoft.Build.Engine;logfile=Build2.log
+
 rem msbuild ..\src\SpNhi.Web\MidExam.Website_deploy.wdproj /t:ResolveReferences;Compile /t:_WPPCopyWebApplication /p:Configuration=Release /p:WebProjectOutputDir=..\..\Release /l:FileLogger,Microsoft.Build.Engine;logfile=Build2.log
 echo on
