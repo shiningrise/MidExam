a5a0f021e49f7e453f19daf848d334274fe9070c
shiningrise <shiningrise@gmail.com>
2013-03-11 22:32:03 +0800

    从dbf数据库取得所有学校信息

---
+++ src/MidExam.Website/frmSchools.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/default.master" AutoEventWireup="true" CodeFile="frmSchools.aspx.cs" Inherits="frmSchools" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" Runat="Server">
+    <asp:GridView ID="gvSchool" runat="server">
+    </asp:GridView>
+</asp:Content>
+
---
+++ src/MidExam.Website/frmSchools.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MidExam.DAL;
+
+public partial class frmSchools : PageBase
+{
+    protected override void AddPermitRoles()
+    {
+        this.AddPermitRole("Teachers");
+        this.AddPermitRole("Students");
+        base.AddPermitRoles();
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            string dbfPath = Server.MapPath("~/Data/Dbf/sysdbf/");
+            string dbfTable = "schools.DBF";
+            this.gvSchool.DataSource = DbfHelper.ToDataTable(dbfPath,dbfTable);
+            this.gvSchool.DataBind();
+        }
+    }
+}
