e19795cf8caeb232a27f75f98d2c105f2b396beb
shiningrise <shiningrise@gmail.com>
2013-03-14 13:19:18 +0800

    添加Api获取学生数据

---
+++ src/MidExam.Website/BmkApi.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MidExam.DAL;
+using Leafing.Data;
+using Newtonsoft.Json;
+
+public partial class BmkApi : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string cmd = Request.QueryString["cmd"] ?? "LoadAll";
+        switch (cmd)
+        {
+            case "LoadAdd":
+                Response.Write(LoadAdd());
+                
+                break;
+            default:
+                Response.Write(LoadAdd());
+                break;
+        }
+        Response.End();
+    }
+
+    private string LoadAdd()
+    {
+        var bmkList = Bmk.Find(Condition.Empty);
+        return JsonConvert.SerializeObject(bmkList);
+    }
+}
