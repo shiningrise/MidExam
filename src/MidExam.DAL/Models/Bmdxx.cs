12ab4e422e84d61dde50443467c3546c3aab8f5d
shiningrise <shiningrise@gmail.com>
2013-03-28 06:53:00 +0800

    编程日志:各功能模块实体类添加完毕

---
+++ src/MidExam.DAL/Models/Bmdxx.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Leafing.Data;
+using Leafing.Data.Definition;
+using Newtonsoft.Json;
+
+namespace MidExam.DAL.Models
+{
+    /// <summary>
+    /// 报名点学校
+    /// </summary>
+    public class Bmdxx
+    {
+        /// <summary>
+        /// 报名学校代码
+        /// </summary>
+        [Length(4)]
+        [Description("报名学校代码")]
+        public string bmxxdm { get; set; }
+
+        /// <summary>
+        /// 报名学校名称
+        /// </summary>
+        [Length(30)]
+        [Description("报名学校名称")]
+        public string bmxxmc { get; set; }
+
+        /// <summary>
+        /// 人数
+        /// </summary>
+        [Description("人数")]
+        public long rs { get; set; }
+
+        [Description("人数")]
+        public long yap_rs { get; set; }
+
+        [Description("人数")]
+        public long wap_rs { get; set; }
+
+        [Description("人数")]
+        public string bmxhq { get; set; }
+
+        [Description("人数")]
+        public string bmxhz { get; set; }
+
+        #region 权限控制
+
+        /// <summary>
+        /// 直接A 0:未启用 1：录入 2核对 3确认
+        /// </summary>
+        [Description("直接A")]
+        [Length(100)]
+        public string FastA { get; set; }
+
+        /// <summary>
+        /// 加分  0:未启用 1：录入 2核对 3确认
+        /// </summary>
+        [Description("加分")]
+        [Length(100)]
+        public string Jiafen { get; set; }
+
+        /// <summary>
+        /// 体育 0:未启用 1：录入 2核对 3确认
+        /// </summary>
+        [Description("体育")]
+        [Length(100)]
+        public string Tiyu { get; set; }
+
+        /// <summary>
+        /// 照顾 0:未启用 1：录入 2核对 3确认
+        /// </summary>
+        [Description("照顾")]
+        [Length(100)]
+        public string Zhaogu { get; set; }
+
+        #endregion
+
+        [Description("公开内容")]
+        public string Wiki1 { get; set; }
+
+        [Description("内部公开内容")]
+        public string Wiki2 { get; set; }
+    }
+}
---
+++ src/MidExam.DAL/Models/Zhaogu.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Leafing.Data;
+using Leafing.Data.Definition;
+using Newtonsoft.Json;
+
+namespace MidExam.DAL.Models
+{
+    /// <summary>
+    /// 政策照顾
+    /// </summary>
+    public class Zhaogu
+    {
+        #region 记录索引
+
+        /// <summary>
+        /// 报名学校代码
+        /// </summary>
+        [Length(4)]
+        [Description("报名学校代码")]
+        public string bmxxdm { get; set; }
+
+        /// <summary>
+        /// 报名表GUID
+        /// </summary>
+        [Description("报名表GUID")]
+        public Guid BmkGuid { get; set; }
+
+        #endregion
+
+        #region 表格主要内容
+
+        /// <summary>
+        /// 报名序号
+        /// </summary>
+        [AllowNull]
+        [Length(9)]
+        [Description("报名序号")]
+        public string bmxh { get; set; }
+
+        [Length(8)]
+        [Description("姓名")]
+        public string xm { get; set; }
+
+        /// <summary>
+        /// 政策照顾类别：
+        /// 降20分： 11烈士子女,12  13
+        /// 降10分：21 22 23
+        /// 降4分：31 32
+        /// </summary>
+        [AllowNull]
+        [Length(100)]
+        [Description("类别")]
+        public string Leibie { get; set; }
+
+        /// <summary>
+        /// 凭据
+        /// </summary>
+        [AllowNull]
+        [Length(100)]
+        [Description("凭据")]
+        public string Pingju { get; set; }
+
+        #endregion
+        
+        #region 审核与备注
+
+        /// <summary>
+        /// 班级审核: 0:待审核，1：退回修改，2，审核不通过，3审核通过
+        /// </summary>
+        [Description("班级审核")]
+        public int Shenhe1 { get; set; }
+
+        /// <summary>
+        /// 学校审核: 0:待审核，1：退回修改，2，审核不通过，3审核通过
+        /// </summary>
+        [Description("学校审核")]
+        public int Shenhe2 { get; set; }
+
+        /// <summary>
+        /// 学校审核: 0:待审核，1：退回修改，2，审核不通过，3审核通过
+        /// </summary>
+        [Description("学校审核")]
+        public int Shenhe3 { get; set; }
+
+        /// <summary>
+        /// 学生备注
+        /// </summary>
+        [AllowNull]
+        [Length(100)]
+        [Description("学生备注")]
+        public string Beizhu0 { get; set; }
+
+        /// <summary>
+        /// 学校备注
+        /// </summary>
+        [AllowNull]
+        [Length(100)]
+        [Description("班级备注")]
+        public string Beizhu1 { get; set; }
+
+        /// <summary>
+        /// 学校备注
+        /// </summary>
+        [AllowNull]
+        [Length(100)]
+        [Description("学校备注")]
+        public string Beizhu2 { get; set; }
+
+        /// <summary>
+        /// 教育局审核
+        /// </summary>
+        [AllowNull]
+        [Length(100)]
+        [Description("教育局审核")]
+        public string Beizhu3 { get; set; }
+
+        #endregion
+
+        /// <summary>
+        /// 未启用，录入，核对,确认
+        /// </summary>
+        [Description("记录状态")]
+        [Length(100)]
+        public string Status { get; set; }
+    }
+}
