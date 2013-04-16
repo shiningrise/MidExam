f4d9ff4369f276781dd7e567566401e7954b24bd
shiningrise <shiningrise@gmail.com>
2013-04-16 17:05:32 +0800

    添加功能:用户列表

---
+++ src/MidExam.Website/User_Index.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/default.master" AutoEventWireup="true"
+    CodeFile="User_Index.aspx.cs" Inherits="User_Index" %>
+
+<%@ Register Assembly="DbEntryMembership" Namespace="DbEntryMembership" TagPrefix="cc1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="Server">
+    <asp:GridView ID="GridView1" runat="server" AllowPaging="True" AutoGenerateColumns="False"
+        DataKeyNames="Id" DataSourceID="DbEntryMembershipUserDataSource1" 
+        onrowcommand="GridView1_RowCommand">
+        <Columns>
+            <asp:BoundField DataField="Id" HeaderText="Id" InsertVisible="False" ReadOnly="True"
+                SortExpression="Id" />
+            <asp:BoundField DataField="UserName" HeaderText="UserName" SortExpression="UserName" />
+            <asp:TemplateField HeaderText="密码初始化" ShowHeader="False">
+                <ItemTemplate>
+                    <asp:LinkButton ID="LinkButton1" runat="server" CausesValidation="False" CommandName="InitPwd"
+                        CommandArgument='<%#Eval("UserName")%>' Text="密码初始化"></asp:LinkButton>
+                </ItemTemplate>
+            </asp:TemplateField>
+        </Columns>
+    </asp:GridView>
+    <cc1:DbEntryMembershipUserDataSource ID="DbEntryMembershipUserDataSource1" runat="server" />
+</asp:Content>
---
+++ src/MidExam.Website/User_Index.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+
+public partial class User_Index : PageBase
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+    {
+        if (e.CommandName == "InitPwd")
+        {
+            //  InitPwd
+            string username = e.CommandArgument.ToString().Trim();
+
+            MembershipUser user = Membership.GetUser(username);
+            if (user == null)
+            {
+                user = Membership.CreateUser(username, username);
+            }
+
+            if (user.IsLockedOut)
+            {
+                user.UnlockUser();
+            }
+            string pwd = user.ResetPassword();
+            if (user.ChangePassword(pwd, username))
+            {
+                //      Response.Write("<script language='javascript'>alert('初始化密码为用户名成功！');</script>");
+
+                //       DataBinder();
+                //LogUtil.Add(
+                //    String.Format("事件:用户{0}成功初始化了学生{1}的密码", Page.User.Identity.Name, user.UserName)
+                //    );
+                //JsUtil.MessageBox(this, "初始化密码为用户名成功！");
+                Succeed("初始化密码为用户名成功！");
+            }
+            else
+            {
+                //       Response.Write("<script language='javascript'>alert('初始化密码为用户名失败！');</script>");
+                //LogUtil.Add(
+                //    String.Format("事件:用户{0}初始化了学生{1}的密码失败", Page.User.Identity.Name, user.UserName)
+                //    );
+                this.Fail("初始化密码为用户名失败！");
+            }
+
+            this.BindData();
+        }
+    }
+
+    private void BindData()
+    {
+        
+    }
+
+}
